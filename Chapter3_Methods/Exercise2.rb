ac2510e207993f585ab6a788f030da69a5917c99
JP Torre <dasma4114@aol.com>
2021-07-20 22:46:12 -0700

    Finished Exercise 2 and 3

---
+++ Chapter3_Methods/Exercise2.rb
@@ -0,0 +1,16 @@
+#What do the following expressions evaluate to?
+
+1. x = 2
+#this returns 2
+
+2. puts x = 2
+#this returns nil 
+
+3. p name = "Joe"
+#this outputs "Joe"
+
+4. four = "four"
+#this returns "four"
+
+5. print something = "nothing"
+#this returns nil
