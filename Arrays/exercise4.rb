4fc96da22ab03096cb4153461ed3f50d0ee67142
JP Torre <jptorre@JPs-MacBook-Pro.T-mobile.com>
2021-07-30 22:48:05 -0700

    Finished exercise 4

---
+++ Arrays/exercise4.rb
@@ -0,0 +1,14 @@
+#What does each method return in the following example?
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+1. arr.index(5)
+
+2. arr.index[5]
+
+3. arr[5]
+
+Answers:
+1. 3 #index method returns the index of the first appearance of the argument
+2. error #index uses parentheses to receive arguments
+3. 8 #calling the 5th index on the array
