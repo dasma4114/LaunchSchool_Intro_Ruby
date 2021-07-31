2ab7321640b8da147adf793b4d23138c8a3799fa
JP Torre <jptorre@JPs-MacBook-Pro.T-mobile.com>
2021-07-31 16:23:34 -0700

    Finished exercise 6

---
+++ Arrays/exercise6.rb
@@ -0,0 +1,8 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+#names['margaret'] = 'jody' => this produces an error because we're trying to
+#access an element of the array with it's key. 
+
+#accessing the element with its index will let us replace the value of that element.
+names[3] = 'jody'
+
+print names
