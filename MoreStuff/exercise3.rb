1b35e07829a8b1a5bfcab6be05d08161ec6ce97e
JP Torre <jptorre@JPs-MacBook-Pro.T-mobile.com>
2021-08-05 11:11:05 -0700

    Finished exercise 3

---
+++ MoreStuff/exercise3.rb
@@ -0,0 +1,5 @@
+#What is exception handling and what problem does it solve?
+
+Exeption handling is the way of writing a program that accounts for possible 
+errors, making sure that the program still runs properly in the event that 
+an error occurs.
