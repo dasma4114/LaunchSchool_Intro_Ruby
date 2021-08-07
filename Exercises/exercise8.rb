e5ef615165dd989068eccabfa31b06322c25021f
JP Torre <dasma4114@aol.com>
2021-08-07 16:36:31 -0700

    Finished exercise 8

---
+++ Exercises/exercise8.rb
@@ -0,0 +1,14 @@
+#Create a Hash, with one key-value pair, using both Ruby syntax styles.
+
+
+my_hash = { # new way
+  a: 1, 
+  b:2, 
+  c:3
+}
+
+my_other_hash = { # old style, using shovel 
+  :a => 1,
+  :b => 2,
+  :c => 3
+}
