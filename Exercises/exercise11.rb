1e56d797d9ed3b2fb0215df767060f053d8fa2f1
JP Torre <jptorre@JPs-MacBook-Pro.T-mobile.com>
2021-08-07 23:11:07 -0700

    Finished exercise 11

---
+++ Exercises/exercise11.rb
@@ -0,0 +1,26 @@
+#Given the following data structures, write a program that copies the information
+#from the array into the empty hash that applies to the correct person.
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+# Expected output:
+#  {
+#    "Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", :phone=>"555-123-4567"},
+#    "Sally Johnson"=>{:email=>"sally@email.com", :address=>"404 Not Found Dr.",  :phone=>"123-234-3454"}
+#  }
+
+contacts["Joe Smith"] = {email: contact_data.first.first,
+  address: contact_data[0][1],
+  phone: contact_data[0].last,
+}
+
+contacts["Sally Johnson"] = {
+  email: contact_data.last.first,
+  address: contact_data.last[1],
+  phone: contact_data.last.last,
+}
+
+p contacts
