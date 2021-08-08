441eddaa08c926ca3247bb2a66444620b3b9cb2f
JP Torre <jptorre@JPs-MacBook-Pro.T-mobile.com>
2021-08-07 22:41:16 -0700

    Finished exercise 10

---
+++ Exercises/exercise10.rb
@@ -0,0 +1,11 @@
+#Can hash values be arrays? Can you have an array of hashes? (give examples)
+#Answer: Yes
+
+arr = [{name: 'jackie', work: 'movie star'}, {name: 'rick', work: 'pawn shop owner'}]
+
+
+my_hash = {
+  teams: ["chicago bulls", "los angeles lakers", "san antonio spurs", "boston celtics"],
+  league: 'nba',
+  game: 'basketball'
+}
